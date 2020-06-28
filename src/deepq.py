a5d39bf19e7424556a71f3c7a1ad69f5f9c1a23f
fmaj7-dev <eturegano@gmail.com>
2020-06-29 00:05:38 +0200

    starting deep-q learning

---
+++ src/deepq.py
@@ -0,0 +1,18 @@
+import random
+
+from PyQt5.QtWidgets import (QGraphicsScene)
+import numpy as np
+
+from car import Car
+from weightset import WeightSet
+
+class DeepQ():
+    def __init__(self, car, map):
+        self.car = car
+        self.map = map
+
+    def run(self):
+        pass
+
+    def nextEpisode(self):
+        pass
---
+++ src/memory.py
@@ -0,0 +1,6 @@
+
+
+class Memory():
+    def __init__(self, max_size = 100):
+        self.max_size = max_size
+        
